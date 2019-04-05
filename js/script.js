890f465e1924992594f7de9984cdf2fc968730c5
Michał Świder <michal.swider@icloud.com>
2019-04-05 12:39:38 +0200

    not-working-ehh

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 'use strict';
 
-const links = document.querySelectorAll('.titles a'); /* Poza główną funkcjątitleClickHandler */
+/* Zmienna z linkami i petla ktora przypisuje nasluch na event musi byc poza glowna funkcja (tittleClickHandler) 
+bo inaczej nie ma sie jak odpalic tzn. nic nie dzialalo bo przypisanie bylo w funkcji, ktorej nic nie wywolywalo */
+const links = document.querySelectorAll('.titles a');
 
+// Tu miales let zamiast for
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
-}
+};
+
 
 
 function titleClickHandler(event) {
     event.preventDefault();
-    const clickedElement = this;
+    const clickedElement = this; //This odnosni sie do tego konkretnego elementu w ktory klikasz, sa odstepstwa ale na ta chwile tyle Ci wystarczy
     console.log('Link was clicked!');
-    console.log(event)
+
 
     /* remove class 'active' from all article links  */
 
@@ -20,26 +24,22 @@ function titleClickHandler(event) {
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
 
+    /* add class 'active' to the clicked link */
     for (let activeArticle of activeArticles) {
         clickedElement.classList.add('active');
     }
-    console.log('clickedElement:', clickedElement);
 
     /* get 'href' attribute from the clicked link */
-
     const articleSelector = clickedElement.getAttribute('href');
-    console.log('href')
+
 
     /* find the correct article using the selector (value of 'href' attribute) */
-
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle)
 
-
     /* add class 'active' to the correct article */
-
     targetArticle.classList.add('active');
+
 
 }
