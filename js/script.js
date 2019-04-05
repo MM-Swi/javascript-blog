400cbeab2a4dcc7b1d224b3b1db06410e5fab7fb
Michał Świder <michal.swider@icloud.com>
2019-04-05 12:26:57 +0200

    5.3-done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const links = document.querySelectorAll('.titles a'); /* Poza główną funkcjątitleClickHandler */
+
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
+
+
 function titleClickHandler(event) {
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
+    console.log(event)
 
     /* remove class 'active' from all article links  */
 
@@ -12,23 +20,26 @@ function titleClickHandler(event) {
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-
     /* add class 'active' to the clicked link */
 
-    let (activeArticle of activeArticles) {
-        activeArticle.classList.add('active')
-    };
+    for (let activeArticle of activeArticles) {
+        clickedElement.classList.add('active');
+    }
     console.log('clickedElement:', clickedElement);
 
     /* get 'href' attribute from the clicked link */
 
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log('href')
+
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle)
+
 
     /* add class 'active' to the correct article */
 
+    targetArticle.classList.add('active');
 
-    const links = document.querySelectorAll('.titles a');
-
-    for (let link of links) {
-        link.addEventListener('click', titleClickHandler);
-    }
+}
